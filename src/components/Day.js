09a07c644298261384acc9784fb5a6c6c4be8112
James Sullivan <jamesfsullivan5@gmail.com>
2018-04-04 12:14:43 -0600

    Add Events component and render with map in Day component

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Event from './Event';
 
 const Day = (props) => {
-  const dayEvents = props.events.length > 0 ?
-    <p>{props.events.name}</p> : null;
+  let dailyEvents;
+
+  if (props.events.length > 0) {
+    dailyEvents = props.events.map(ev => (
+      <Event
+        key={ev.name}
+        name={ev.name}
+      />
+    ));
+  }
 
   return (
     <div className={`day ${props.weekend ? 'weekend' : null}`}>
       <p>{props.day}</p>
-      {dayEvents}
+      {dailyEvents || null}
     </div>
   );
 };
---
+++ src/components/Event.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Event = props => (
+  <div>
+    {props.name}
+  </div>
+);
+
+export default Event;
