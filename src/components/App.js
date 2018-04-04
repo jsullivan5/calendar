daa8a8e0ca27020d5704181996794e21808e3914
James Sullivan <jamesfsullivan5@gmail.com>
2018-04-04 14:42:57 -0600

    Add contructor with default changeMonth property in App

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,22 @@ import Calendar from './Calendar';
 import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      changeMonth: () => null,
+    };
+    this.getFunc = this.getFunc.bind(this);
+  }
+  getFunc(func) {
+    this.setState({ changeMonth: func });
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Calendar />
+        <Header changeMonth={this.state.changeMonth} />
+        <Calendar getFunc={this.getFunc} />
       </div>
     );
   }
