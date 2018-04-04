344162571359bbf784111c6e65da934da0cb1530
James Sullivan <jamesfsullivan5@gmail.com>
2018-04-04 11:57:45 -0600

    Move renderCal method outside of class and add addClass for weekend background color

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import Day from './Day';
+import { october, november } from '../mockCalData';
+
+const addClass = (index) => {
+  if (index % 7 === 0 || (index - 1) % 7 === 0) {
+    return true;
+  }
+  return false;
+};
+
+const renderCal = () => october.map((day, i) => {
+  const calendarDay = i + 1;
+  const isWeekend = addClass(calendarDay);
+
+  return (
+    <Day
+      key={`oct-${i}`}
+      weekend={isWeekend}
+      day={calendarDay}
+      events={day.events}
+    />
+  );
+});
 
 class Calendar extends React.Component {
   constructor() {
     super();
     this.state = {
-      days: [],
+      october: [],
+      november: [],
     };
   }
   componentDidMount() {
-    this.renderCal();
-  }
-
-  renderCal() {
-    const dayCount = 42;
-    const daysArr = [];
-
-    for (let i = 0; i < dayCount; i += 1) {
-      daysArr.push(<Day day={i} />);
-    }
-
-    this.setState({ days: daysArr });
+    this.setState({
+      october,
+      november,
+    });
   }
 
   render() {
+    const calDays = renderCal();
+
     return (
       <main className="grid">
-        {this.state.days}
+        {calDays}
       </main>
     );
   }
