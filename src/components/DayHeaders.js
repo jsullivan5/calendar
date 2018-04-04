06142d826db12dbb3f65c14cee60df71aaff8e59
James Sullivan <jamesfsullivan5@gmail.com>
2018-04-04 11:10:43 -0600

    Add DayHeaders component to Header component

---
+++ src/components/DayHeaders.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DayHeaders = () => (
+  <div className="grid">
+    <p>Sun</p>
+    <p>Mon</p>
+    <p>Tue</p>
+    <p>Wed</p>
+    <p>Thu</p>
+    <p>Fri</p>
+    <p>Sat</p>
+  </div>
+);
+
+export default DayHeaders;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import DayHeaders from './DayHeaders';
+
 import './styles/App.css';
 
 const Header = () => (
   <header>
     <h1>October</h1>
+    <DayHeaders />
   </header>
 );
 
