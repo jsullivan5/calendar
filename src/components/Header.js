8ec9c4c7b81c51e1a2bcb2281584a2859532bd1e
James Sullivan <jamesfsullivan5@gmail.com>
2018-04-04 14:43:56 -0600

    Add nav with buttons to cycle through months in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DayHeaders from './DayHeaders';
 
 import './styles/App.css';
 
-const Header = () => (
+const Header = ({ changeMonth }) => (
   <header>
+    <nav className="nav">
+      <button onClick={() => changeMonth('back')}>
+        Prev.
+      </button>
+      <button onClick={() => changeMonth('forward')}>
+        Next
+      </button>
+    </nav>
     <h1>October</h1>
     <DayHeaders />
   </header>
 );
+
+Header.propTypes = {
+  changeMonth: PropTypes.func.isRequired,
+};
 
 export default Header;
